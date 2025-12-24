912ed940e427d248b02cba1d1c96d37e353fa292
waseem00096 <waseemhassan00096@gmail.com>
2025-12-24 16:53:52 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ resource "helm_release" "prometheus_stack" {
   name       = "kube-stack"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
-  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+  namespace  = kubernetes_namespace_v1.monitoring.metadata[0].name
 
   # Security: Skip TLS verify for bare metal if needed
   # Equivalent to --kube-insecure-skip-tls-verify
