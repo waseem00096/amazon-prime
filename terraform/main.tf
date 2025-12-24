7646806ef377026b4e0984083936630075764656
waseem00096 <waseemhassan00096@gmail.com>
2025-12-24 19:25:59 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,29 @@
-# Use 'data' instead of 'resource' if the namespace is already there
+# 1. Reference the existing monitoring namespace
 data "kubernetes_namespace_v1" "monitoring" {
   metadata {
     name = "monitoring"
   }
 }
 
+# 2. Deploy Metrics Server (REQUIRED for CPU/Memory graphs)
+resource "helm_release" "metrics_server" {
+  name       = "metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  namespace  = "kube-system"
+
+  # Bare-metal fix: Metrics server must ignore insecure kubelet certs
+  set {
+    name  = "args"
+    value = "{--kubelet-insecure-tls,--kubelet-preferred-address-types=InternalIP}"
+  }
+}
+
+# 3. Deploy Prometheus/Grafana Stack
 resource "helm_release" "prometheus_stack" {
   name       = "kube-stack"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
-  
-  # Point to the data source instead of the resource
   namespace  = data.kubernetes_namespace_v1.monitoring.metadata[0].name
 
   set = [
