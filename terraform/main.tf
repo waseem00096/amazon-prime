7156cabfd97cd08f4de6f80f9e8bfd5e4892b946
waseem00096 <waseemhassan00096@gmail.com>
2025-12-24 17:41:54 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,3 @@
-# Use _v1 to resolve the deprecation warning
 resource "kubernetes_namespace_v1" "monitoring" {
   metadata {
     name = "monitoring"
@@ -9,8 +8,6 @@ resource "helm_release" "prometheus_stack" {
   name       = "kube-stack"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
-  
-  # Ensure this reference matches the new v1 resource name
   namespace  = kubernetes_namespace_v1.monitoring.metadata[0].name
 
   set {
